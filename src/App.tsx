a5e33cd78c310f808b0f2fdbb8ca9528dc95854e
Banerjee Prodipta <prodiptaj@live.com>
2022-03-14 20:32:22 +0600

    style:(heading and p tag) use typography to create h2 header and p tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,16 @@ import './styles/tailwind.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-100">
+    <div
+      className="min-h-screen"
+      style={{
+        background: 'linear-gradient(113.49deg, #984D38 -30.3%, #181E41 58.12%)',
+      }}
+    >
       <div className="container p-6 mx-auto">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center prose-p:font-medium text-white/50 prose-headings:text-white prose-h2:font-[Roboto] prose-h2:font-bold prose-h2:text-4xl">
+          <p>hi</p>
+          <h2>hi</h2>
           <BrowserRouter>
             <Routes />
           </BrowserRouter>
