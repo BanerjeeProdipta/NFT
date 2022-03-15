605be74e4211eec09b331fdde168a65b5c605556
Banerjee Prodipta <prodiptaj@live.com>
2022-03-15 22:14:47 +0600

    fix:(font size)

--- src/pages/landing/AboutUs.tsx
+++ src/pages/landing/AboutUs.tsx
@@ -14,7 +14,7 @@ function AboutUs() {
       <div className="flex flex-col items-center justify-between space-y-6 md:flex-row md:space-x-8 md:space-y-0">
         <img src="image/aboutUs/1.png" alt="about us 1" className="max-w-md md:w-1/2" />
         <div className="space-y-4 text-left lg:w-1/2">
-          <h3 className="text-lg font-bold">Get Popular NFT</h3>
+          <h3 className="text-3xl font-bold">Get Popular NFT</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. </p>
           <PrimaryButton text="Show more" />
 
