5557461fbe1073714ce0bdd0b9d73e34c24b4525
Banerjee Prodipta <prodiptaj@live.com>
2022-03-15 21:53:16 +0600

    feat:(about us) button name change to PrimaryButton & about us section completed

--- src/components/ui/PrimaryButton.tsx
+++ src/components/ui/PrimaryButton.tsx
@@ -2,10 +2,10 @@ interface props{
   text:string
 }
 
-function Button({ text }:props) {
+function PrimaryButton({ text }:props) {
   return (
     <button type="button" className="w-auto px-4 py-2 text-white transition delay-200 rounded-lg bg-gradient-to-bl from-primaryLight to-primaryDark hover:bg-gradient-to-tr">{text}</button>
   )
 }
 
-export default Button
+export default PrimaryButton
