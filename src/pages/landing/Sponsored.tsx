0876f20a7d078c0cf1eeb2ff02a3de5d90c175e0
Banerjee Prodipta <prodiptaj@live.com>
2022-03-16 15:37:18 +0600

    fix:(responsiveness)

--- src/pages/landing/Sponsored.tsx
+++ src/pages/landing/Sponsored.tsx
@@ -1,9 +1,9 @@
 function Sponsored() {
   return (
     <div className="flex flex-col items-center justify-between space-y-12 lg:space-y-0 lg:flex-row">
-      <img src="/image/sponsored/Binance.svg" alt="Binance logo" />
-      <img src="/image/sponsored/Ethereum.svg" alt="Ethereum logo" />
-      <img src="/image/sponsored/Blockchain.svg" alt="Blockchain logo" />
+      <img src="/image/sponsored/Binance.svg" alt="Binance logo" className="object-scale-down xs:object-fill" />
+      <img src="/image/sponsored/Ethereum.svg" alt="Ethereum logo" className="object-scale-down xs:object-fill" />
+      <img src="/image/sponsored/Blockchain.svg" alt="Blockchain logo" className="object-scale-down xs:object-fill" />
     </div>
   )
 }
