d0f24441a484dd01bcd9f3e2b951cbebf3f30f26
Banerjee Prodipta <prodiptaj@live.com>
2022-03-16 12:38:39 +0600

    style:(nav) navbar bg frosty effect ans mobile view menu bg color add

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/ui/Navbar'
 import Routes from './routes/Routes'
 import './styles/global.css'
 import './styles/tailwind.css'
@@ -11,15 +12,26 @@ function App() {
         background: 'linear-gradient(113.49deg, #984D38 -30.3%, #181E41 58.12%)',
       }}
     >
-      <div className="container p-4 mx-auto">
-        <div className="flex flex-col items-center justify-center">
-          <div className="max-w-6xl prose-p:font-medium prose-p:text-xl text-white/50 prose-headings:text-white prose-h2:font-[Roboto] prose-h2:font-bold prose-h2:text-4xl">
-            <BrowserRouter>
-              <Routes />
-            </BrowserRouter>
+      <BrowserRouter>
+        <div className="prose-p:font-medium prose-p:text-xl text-white/50 prose-headings:text-white prose-h2:font-[Roboto] prose-h2:font-bold prose-h2:text-4xl">
+
+          <div
+            className="fixed w-full"
+            style={{ backdropFilter: 'blur(6px)' }}
+          >
+            <Navbar />
+          </div>
+
+          <div className="container p-4 mx-auto">
+            <div className="flex flex-col items-center justify-center">
+              <div className="max-w-6xl mt-10 lg:mt-24">
+                <Routes />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+
+      </BrowserRouter>
     </div>
   )
 }
