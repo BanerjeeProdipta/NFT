5656ffa3ef9ee03189de68339d1f589da9ca501f
Banerjee Prodipta <prodiptaj@live.com>
2022-03-18 21:24:55 +0600

    fix:(typo name)

--- src/utils/data/featuredArts.ts
+++ src/utils/data/featuredArts.ts
@@ -3,19 +3,19 @@ import { ArtDataType } from '../../types/artDataType'
 const featuredArts:ArtDataType[] = [
   {
     img: 'image/collection/collection1.png',
-    author: 'Jonny',
+    author: 'John Wick',
     price: '0.005ETH',
     name: 'Yellow Painting',
   },
   {
     img: 'image/collection/collection2.png',
-    author: 'Jonny',
+    author: 'John Wick',
     price: '0.005ETH',
     name: 'Yellow Painting',
   },
   {
     img: 'image/collection/collection3.png',
-    author: 'Jonny',
+    author: 'John Wick',
     price: '0.005ETH',
     name: 'Yellow Painting',
   },
